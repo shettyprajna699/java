10930c586ccf0235ae9826fde0e915e7b2a0a816
shettyprajna699 <shettyprajna699@gmail.com>
2024-02-12 10:17:02 +0530

    Revert "Revert "second project""
    This reverts commit 77006953d6266c9a488e2595bbc5055d5a70983b.

---
+++ src/main/java/com/repository/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.entity.Student;
+
+public interface StudentRepository extends JpaRepository<Student,Long> {
+	
+}
